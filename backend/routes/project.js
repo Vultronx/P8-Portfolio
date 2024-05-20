d3526d0cca2ca7e9c0ac7a2c89a515a92a202ec3
Vultronx <142888084+Vultronx@users.noreply.github.com>
2024-05-20 03:54:43 +0200

    Update

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -3,12 +3,11 @@ const router = express.Router();
 
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
-const multerSharp = require('../middleware/sharp-config');
 
 const projectCtrl = require('../controllers/project');
 
 router.get('/', projectCtrl.getAllProject);
-router.post('/', auth, multerSharp, projectCtrl.createProject);
+router.post('/', auth, multer, projectCtrl.createProject);
 router.get('/:id', projectCtrl.getOneProject);
 router.put('/:id', auth, multer, projectCtrl.modifyProject);
 router.delete('/:id', auth, projectCtrl.deleteProject);
